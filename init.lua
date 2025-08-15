de3b20aeafbdfff1d2fb05bb12b43f4db0a6ea38
WeaponizedSchizophrenia <laurynasramankevicius2@gmail.com>
2025-08-15 12:31:26 +0300

    Add csharp_ls

--- init.lua
+++ init.lua
@@ -694,6 +694,7 @@ require('lazy').setup({
         -- pyright = {},
         rust_analyzer = {},
         neocmake = {},
+        csharp_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
